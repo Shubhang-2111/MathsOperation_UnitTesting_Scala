aebf335648fe6d3640d16963adeea15ac494c9c2
Shubhang-2111 <singhshubhang1920@gmail.com>
2023-11-06 23:21:51 +0530

    changed to class

--- src/main/scala/MathsOperation.scala
+++ src/main/scala/MathsOperation.scala
@@ -3,7 +3,7 @@ import scala.util.Try
 // this is update
 object MathsOperation {
   def add(num1:Int,num2:Int):Int = {
-    num1+num2;
+    num1+num2
   }
 
   def subtract(num1: Int, num2: Int): Int = {
@@ -11,11 +11,11 @@ object MathsOperation {
   }
 
   def multiply(num1: Int, num2: Int): Int = {
-    num1*num2;
+    num1*num2
   }
 
   def divide(num1: Int, num2: Int): Int = {
-    num1/num2;
+    num1/num2
   }
 
 }
